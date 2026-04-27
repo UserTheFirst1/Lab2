2c80b603c02649d24aa14cfbd619d6b772a0e218
Arafat <alarafatremin.25@ichat.sp.edu.sg>
2026-04-27 15:32:59 +0800

    gaga

--- Lab2.py
+++ Lab2.py
@@ -1,16 +1,66 @@
-def calculate_bmi(height, weight, bmi):
-    print("Height= " + str(height))
-    print("Weight= " + str(weight))
-    bmi = weight / (height * height)
-    print("BMI= " + str(bmi))
-    if bmi < 18.5:
-        print("Underweight")
-    elif bmi >= 18.5 and bmi < 25:
-        print("Normal Weight")
-    elif bmi >= 25 and bmi < 30:
-        print("Overweight")
+def main():
+    display_main_menu()
+    float_list = get_user_input()
+    sort_temperature(float_list)
+    calc_average(float_list)
+    find_min_max(float_list)
+    calc_median_temperature(float_list)
+
+def display_main_menu():
+    print("Enter some numbers separated by commas (eg. 5, 65, 34)")
 
 
+def get_user_input():
+    numlist = input("Enter numbers: ")
+    splitlist = numlist.split(",")
+
+    float_list = []
+    for count in splitlist:
+        float_list.append(float(count.strip()))  
+
+    return float_list
 
 
-calculate_bmi(weight=57,height = 1.73,bmi=0)
+def sort_temperature(float_list):
+    float_list.sort()
+    return float_list
+    
+
+
+def calc_average(float_list):
+    if len(float_list) == 0:
+        return 0
+
+    average = sum(float_list) / len(float_list)
+    print("The Average Is " + str(average))
+    return average
+
+
+def find_min_max(float_list):
+    min_temp = min(float_list)
+    max_temp = max(float_list)
+
+    print("Min Temperature is " + str(min_temp))  
+    print("Max Temperature is " + str(max_temp)) 
+
+    return [min_temp, max_temp]
+
+
+def calc_median_temperature(float_list):  
+    sorted_list = sort_temperature(float_list)
+    length = len(sorted_list)
+
+    if length == 0:
+        return 0
+
+    if length % 2 == 0:
+        mid1 = length // 2 - 1
+        mid2 = length // 2
+        median = (sorted_list[mid1] + sorted_list[mid2]) / 2
+    else:
+        mid = length // 2
+        median = sorted_list[mid]
+
+    return median
+
+main()
