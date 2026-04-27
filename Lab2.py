6029cc46f2155f5c9536bdade167e51366e122da
Arafat <alarafatremin.25@ichat.sp.edu.sg>
2026-04-27 13:57:38 +0800

    JAja

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,16 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def calculate_bmi(height, weight, bmi):
+    print("Height= " + str(height))
+    print("Weight= " + str(weight))
+    bmi = weight / (height * height)
+    print("BMI= " + str(bmi))
+    if bmi < 18.5:
+        print("Underweight")
+    elif bmi >= 18.5 and bmi < 25:
+        print("Normal Weight")
+    elif bmi >= 25 and bmi < 30:
+        print("Overweight")
+
+
+
+
+calculate_bmi(weight=57,height = 1.73,bmi=0)
